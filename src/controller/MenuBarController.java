6a38dbb9c860ebb793bd36a347392d3acf0137dc
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-08-14 12:49:27 +0530

    Menubar created

--- src/controller/MenuBarController.java
+++ src/controller/MenuBarController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.layout.AnchorPane;
+
 public class MenuBarController {
+    public JFXButton menuDashboardBtn;
+
+    public AnchorPane playGroundAnchorPane;
+    public Label userNameLbl;
+    public MenuButton menuOrdersBtn;
+    public MenuButton menuCustomersBtn;
+    public MenuButton menuItemsBtn;
+    public Label dateLbl;
+    public Label timeLbl;
+    public JFXButton menuLogoutBtn;
 }
