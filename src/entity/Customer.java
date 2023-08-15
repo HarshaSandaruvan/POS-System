3f79e250b79c65d120c5095a141bc443ec9f3407
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-08-16 00:10:38 +0530

    Customer Entity Created

---
+++ src/entity/Customer.java
@@ -0,0 +1,17 @@
+package entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Customer {
+    private String customerId;
+    private String firstName;
+    private String lastName;
+    private String nic;
+    private String address;
+    private int contactNo;
+}
