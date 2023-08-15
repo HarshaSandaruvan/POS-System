a80f2960f8e651d7ac98d6e1ad7565473e42d657
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-08-16 00:15:30 +0530

    CustomerBO and CustomerDAO Created

---
+++ src/bo/custom/CustomerBO.java
@@ -0,0 +1,6 @@
+package bo.custom;
+
+import bo.SuperBO;
+
+public interface CustomerBO extends SuperBO {
+}
---
+++ src/dao/custom/impl/CustomerDAOImpl.java
@@ -0,0 +1,6 @@
+package dao.custom.impl;
+
+import dao.custom.CustomerDAO;
+
+public class CustomerDAOImpl implements CustomerDAO {
+}
