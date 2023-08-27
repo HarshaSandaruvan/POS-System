08419fdad82b449a98686f4eb3de29c6e75b2885
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-08-27 13:03:25 +0530

    OrderDAO Save method create

--- src/dao/custom/impl/OrderDAOImpl.java
+++ src/dao/custom/impl/OrderDAOImpl.java
@@ -1,11 +1,28 @@
 package dao.custom.impl;
 
+import dao.CrudUtil;
 import dao.custom.OrderDAO;
 import entity.Orders;
+
+import java.sql.SQLException;
 
 public class OrderDAOImpl implements OrderDAO {
     @Override
     public boolean saveOrder(Orders orders) {
-        return false;
+        try {
+            return CrudUtil.executeUpdate("INSERT INTO orders VALUES(?,?,?,?,?,?)",
+                    orders.getOrderID(),
+                    orders.getCustomerID(),
+                    orders.getTime(),
+                    orders.getDate(),
+                    orders.getCashierID(),
+                    orders.getTotal()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
